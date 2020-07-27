e5572402e52bd242f996a2f2bf088a47a67af44a
Brian Caulfield <bcaulfield@bcaulfield-a01.vmware.com>
2020-07-27 15:16:03 -0500

    initial commit

---
+++ init.vim
@@ -0,0 +1,178 @@
+syntax on
+set noerrorbells			"Turns off error sounds
+set tabstop=4 softtabstop=4 " Sets the tab size to 4 spaces
+set shiftwidth=4 
+set smartindent				" smart indenting when possible
+set nu						" sets numbers on line to be on
+set nowrap					" turns off line wrapping
+set smartcase				" if search is all lowercase then ignore case until use upper case letter
+set noswapfile				" don't create swap files
+set nobackup				" Don't create backup files
+set undodir=~/.config/nvim/undodir
+set undofile				" use undo files
+set incsearch				" Use incremental search
+set relativenumber			" relative line numbes
+set nohlsearch				" no highlight on search
+set mouse=a
+
+
+" neovim general configuration
+set splitright
+set splitbelow
+let $NVIM_TUI_ENABLE_TRUE_COLOR=1
+let mapleader = " "
+
+"" Move between windows
+nnoremap <leader>h :wincmd h<CR>
+nnoremap <leader>j :wincmd j<CR>
+nnoremap <leader>k :wincmd k<CR>
+nnoremap <leader>l :wincmd l<CR>
+nnoremap <leader>v :wincmd v<CR>
+nnoremap <leader>s :wincmd s<CR>
+" terminal navigation
+nnoremap <leader>t :split<CR>:res 10<CR>:terminal<CR>
+tnoremap <ESC> <C-\><C-n> <CR>
+"tnoremap <A-h> <C-\><C-n>  :wincmd h<CR>
+"tnoremap <A-j> <C-\><C-n>  :wincmd j<CR>
+"tnoremap <A-k>  <C-\><C-n> :wincmd k<CR>
+"tnoremap <A-l>  <C-\><C-n>  :wincmd l<CR>
+
+imap jj <ESC>
+let loaded_matchparen = 1
+
+"" Move lines up and down
+nnoremap <A-j> :m .+1<CR>==
+nnoremap <A-k> :m .-2<CR>==
+inoremap <A-j> <Esc>:m .+1<CR>==gi
+inoremap <A-k> <Esc>:m .-2<CR>==gi
+vnoremap <A-j> :m '>+1<CR>gv=gv
+vnoremap <A-k> :m '<-2<CR>gv=gv
+
+nnoremap <silent><S-j> :set paste<CR>m`o<Esc>``:set nopaste<CR>
+nnoremap <silent><S-k> :set paste<CR>m`O<Esc>``:set nopaste<CR>
+
+"" Pane resize
+noremap <leader>+ :resize +5<CR>
+noremap <leader>- :resize -5<CR>
+noremap <leader>< :vertical:resize -5<CR>
+noremap <leader>> :vertical:resize +5<CR>
+
+""Buffer control
+:nnoremap <Tab> :bnext<CR>
+:nnoremap <S-Tab> :bprevious<CR>
+
+"Plugin installation
+call plug#begin('~/.local/share/nvim/plugged')
+
+Plug 'vim-utils/vim-man'
+Plug 'joshdick/onedark.vim'
+Plug 'rakr/vim-one'
+Plug 'morhetz/gruvbox'
+Plug 'scrooloose/nerdtree'
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'clangd/coc-clangd'
+Plug 'airblade/vim-rooter'
+Plug 'jiangmiao/auto-pairs'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'preservim/nerdcommenter'
+Plug 'airblade/vim-gitgutter'
+Plug 'ayu-theme/ayu-vim'
+Plug 'drewtempelmeyer/palenight.vim'
+
+call plug#end()
+
+
+" ripgrep configuration
+"let g:rg_command = 'rg --vimgrep -S' " enables smartcasing in ripgrep
+"if executable('rg')
+"	let g:rg_derive_root='true'
+"endif
+
+
+" fzf Config
+nnoremap <C-p> <Esc><Esc>:Files!<CR>
+nnoremap <C-f> <Esc><Esc>:BLines<CR>
+nnoremap <C-g> :Rg<CR> 
+nnoremap <C-b> :Buffers<CR>
+let g:fzf_action = {
+  \ 'ctrl-t': 'tab split',
+  \ 'ctrl-s': 'split',
+  \ 'ctrl-v': 'vsplit'
+  \}
+
+" coc configuration
+"" Tab Completion
+inoremap <silent><expr> <TAB>
+      \ pumvisible() ? "\<C-n>" :
+      \ <SID>check_back_space() ? "\<TAB>" :
+      \ coc#refresh()
+inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+
+function! s:check_back_space() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+"" Code Navigation
+nmap <leader>gd <Plug>(coc-definition)
+nmap <leader>gy <Plug>(coc-type-definition)
+nmap <leader>gi <Plug>(coc-implementation)
+nmap <leader>gr <Plug>(coc-references)
+nmap <leader>rr <Plug>(coc-rename)
+nmap <leader>g[ <Plug>(coc-diagnostic-prev)
+nmap <leader>g] <Plug>(coc-diagnostic-next)
+nmap <silent> <leader>gp <Plug>(coc-diagnostic-prev-error)
+nmap <silent> <leader>gn <Plug>(coc-diagnostic-next-error)
+nnoremap <leader>cr :CocRestart
+
+
+" Airline Configuration
+let g:airline#extensions#tabline#enabled = 1
+let g:airline_theme='deus'
+
+
+"One dark setup
+" g:onedark_terminal_italics=1
+" colorscheme onedark
+
+" Gruvbox setup
+"let g:gruvbox_italic=1
+"let g:gruvbox_bold=1
+"let g:gruvbox_contrast_dark='medium' "soft medium hard
+"set background=dark
+"colorscheme gruvbox
+
+"set background=light
+"let g:one_allow_italics=1
+"colorscheme one
+
+
+"set termguicolors
+"let ayucolor="dark"
+"colorscheme ayu
+if (has("nvim"))
+  "For Neovim 0.1.3 and 0.1.4 < https://github.com/neovim/neovim/pull/2198 >
+  let $NVIM_TUI_ENABLE_TRUE_COLOR=1
+endif
+
+"For Neovim > 0.1.5 and Vim > patch 7.4.1799 < https://github.com/vim/vim/commit/61be73bb0f965a895bfb064ea3e55476ac175162 >
+"Based on Vim patch 7.4.1770 (`guicolors` option) < https://github.com/vim/vim/commit/8a633e3427b47286869aa4b96f2bfc1fe65b25cd >
+" < https://github.com/neovim/neovim/wiki/Following-HEAD#20160511 >
+if (has("termguicolors"))
+  set termguicolors
+endif
+
+let g:palenight_terminal_italics=1
+set background=dark
+colorscheme palenight
+
+
+" NerdTree Setup
+let g:NERDTreeShowHidden = 1
+let g:NERDTreeMinimalUI = 1
+let g:NERDTreeIgnore = []
+let g:NERDTreeStatusline = ''
+nnoremap <silent> <C-n> :NERDTreeToggle<CR>
---
+++ vim_setup.sh
@@ -0,0 +1,283 @@
+#! /bin/bash 
+
+# use these environment variables
+user_home_dir='~'
+user_bin_dir='~/bin'
+root_access=false
+
+# set up the platform os
+platform=''
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	platform="linux"
+elif [["$OSTYPE" == "darwin"* ]]; then
+	platform="mac"
+else
+	echo "This script does not support this OS"
+	exit 1
+fi
+
+# check for root access
+echo "Checking for root access"
+if [[ $(id -u) -ne 0 ]]; then
+	echo "No root access detected"
+	root_access=false
+else
+	echo "root access detected"
+	root_access=true
+fi
+
+function check_install(){
+	# checking installation
+	echo "Checking installation..."
+	if which $1 &> /dev/null; then
+		echo "installation successful!"
+	else
+		echo "Something went wrong during the process :( "
+		exit 1
+	fi
+
+}
+
+# $1 is the name of the program, $2 is the bin path
+function add_to_path() {
+	echo "\nAdding $1 to PATH" >> $user_home_dir/.bashrc
+	echo "PATH=$PATH:$2" >> $user_home_dir/.bashrc
+	source $user_home_dir/.bashrc
+}
+
+function create_user_bin(){
+	if [ -d $user_bin_dir ]; then
+		echo "user bin dir already created"
+		return
+	fi
+
+	echo "Creating a user bin directory in $user_bin_dir"
+	mkdir -p $user_bin_dir
+}
+
+function install_neovim() {
+	if which nvim &> /dev/null; then
+		echo "Neovim already installed!"
+		return
+	fi
+
+	echo "Installing Neovim"
+	
+	if [ $platform == "mac" ]; then 
+		brew install veovim
+
+	elif [ $platform == "linux" && ! $root_access ]; then
+		# user does not have root access, install via wget
+		nvim_url="https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz"
+		filename='nvim-linux64'
+
+		# download file
+		echo "Downloading Neovim from github"
+		curl -o $filename -L $nvim_url
+		
+		# extract file to the bin directory 	
+		echo "extracting to bin directory located at $user_bin_dir"
+		tar -xzf $filename -C $user_bin_dir
+
+		# add that file to the path
+		echo "Installing on path"
+		add_to_path "nvim" $user_bin_dir/$filename/bin
+		echo "alias vim='nvim'" >> $user_home_dir/.bashrc
+
+		rm -rf $filename
+	else
+		# have root access just use package manager
+		sudo apt install neovim
+	fi
+	
+	check_install nvim
+
+	echo "Cleaning up nvim install"
+	rm -rf $filename
+}
+
+function install_nvim_config_file(){
+	if [ ! -f ./init.vim ]; then
+		echo "Could not find config fie. Be sure to run this script directly from the repo directory";
+		exit 1
+	fi
+	if [ ! -d $user_home_dir/.config/nvim ]; then
+		echo "Creating nvim config directory"
+		mkdir -p $user_home_dir/.config/nvim
+	fi
+
+	echo "installing nvim config file"
+	cp ./init.vim $user_home_dir/.config/nvim
+	
+	if [ $? == 1 ]; then
+		echo "Could not copy config file"
+		exit 1
+	fi
+}
+
+function install_nvim_plugin_manager(){
+	echo "Installing Plugin Manager"
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+}
+
+function install_rg() {
+	echo "Installing ripgrep"
+	if which rg &> /dev/null; then
+		echo "ripgrep already installed"
+	fi
+
+	if [ $platform == "mac" ]; then
+		brew install ripgrep
+		 
+	elif [ $platform == "linux" && ! $root_access ]; then
+		# user does not have root access, install via wget
+		rg_url="https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep-12.1.1-x86_64-unknown-linux-musl.tar.gz"
+		filename='ripgrep-12.1.1-x86_64-unkown-linux-musl'
+
+		# download file
+		echo "Downloading ripgrep from github"
+		curl -o $filename -L $rg_url
+		
+		# extract file to the bin directory 	
+		echo "extracting to bin directory located at $user_bin_dir"
+		tar -xzf $filename -C $user_bin_dir
+
+		# add that file to the path
+		echo "Installing on path"
+		add_to_path "ripgrep" $user_bin_dir/$filename/bin
+
+		rm -rf $filename
+	else
+		# have root access just use package manager
+		sudo apt install ripgrep
+	fi
+	
+	check_install rg
+
+	echo "Cleaning up ripgrep install"
+	rm -rf $filename
+}
+
+# CoC requires nodejs so we need to install that first
+function install_node_js(){
+	echo "installing node-js for the CoC vim plugin"
+	if which node &> /dev/null; then
+		echo "node-js is already installed"
+		return
+	fi
+	
+	if [$platform == "mac" || $root_access ]; then
+		curl -sL install-node.now.sh/lts | bash
+	elif [ $platform == "linux" && ! $root_access ]; then 
+		node_url='https://nodejs.org/download/release/latest/node-v14.6.0-linux-x64.tar.gz'
+		filename='node-v14.6.0-linux-x64'
+		
+		echo "downloading node-js binaries"
+		curl -O $filename -L $node_url
+
+		echo "extracting to bin directory located at $user_bin_dir"
+		tar -xzf $filename -C $user_bin_dir
+
+		echo "altering path"
+		add_to_path "node-js" $user_bin_dir/$filename/bin
+
+	else
+		echo "I'm confused about this whole node thing. Exiting."
+		exit 1
+	fi
+	
+	check_install node
+
+	echo "Cleaning up node-js install"
+	rm -rf $filename
+}
+
+function install_clangd(){
+	echo "Installing clangd for the coc c/c++ plugin"
+	if which clangd &> /dev/null; then
+		echo "clangd is already installed"
+		return
+	fi
+	
+	if [$platform == "mac" ]; then
+		brew install clangd
+		add_to_path "clangd" /usr/local/opt/llvm/bin
+
+	elif [ $platform == "linux" && ! $root_access ]; then 
+		clangd_url='https://github.com/clangd/clangd/releases/download/10.0.0/clangd-linux-10.0.0.zip'
+		filename='clangd-linux-10.0.0.zip'
+		
+		echo "downloading clangd binaries"
+		wget -cO - $clangd_url > $filename
+
+		echo "extracting to bin directory located at $user_bin_dir"
+		unzip $filename -d $user_bin_dir
+		
+		clangd_name="clangd_10.0.0"
+
+		echo "altering path"
+		add_to_path "clangd" $user_bin_dir/$clangd_name/bin
+	elif [$platform == "linux" && $root_access ]; then
+		sudo apt install clangd
+	fi
+	
+	check_install clangd
+
+	rm -rf $filename $clangd_name
+}
+
+
+
+#vim_setup --home-dir {arg} --bin-dir-location {arg}
+#vim_setup -h ~/some/path -b some/path/bin
+	#if --no-root-access is provided, then use wget to install things
+		#also use a bin dir in the home directory
+			#if this arg is set, then we use this arg as the home dir - otherwise it uses ~
+
+USAGE="\
+Run this program from within the git repo. 
+./vim_setup.sh [-h home-dir] [-b bin-dir]
+	-d		specifies the user's home directory to use. Defaults to ~
+	-b		specifies the user's bin directory. Use this if you don't have 
+			root priveleges defaults to ~/bin 
+
+If you have root access, run this script with sudo to use the OS package manager.
+Without root access, the script will attempt to install all binaries into the specified
+bin directory. 
+"
+
+function main(){
+	while getopts d:b:h: option
+		do
+			case "${option}" in
+				d) user_home_dir=${OPTARG};;
+				b) user_bin_dir=${OPTARG};;
+				h) echo $USAGE; exit 0;;
+			esac
+		done
+
+	echo "Running with user home dir = $user_home_dir and bin dir = $user_bin_dir"
+
+	install_neovim
+	install_nvim_config_file
+	install_nvim_plugin_manager
+	install_rg
+	install_node_js
+	install_clangd
+	
+	echo "Installation Finished. Don't forget to go into Neovim and run :PlugInstall to get all of the fancy plugins!"
+	exit 0
+}
+
+main "$@"
+
+
+
+
+
+
+
+
+
+
+
